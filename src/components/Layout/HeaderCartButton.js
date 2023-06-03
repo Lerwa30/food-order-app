2b4d0873cd4cfac658db17271f4aa17f08c2f444
Leroy <leroypawlick@gmail.com>
2023-06-03 11:51:06 -0700

    added logic to add items to cart and display in modal

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,24 @@
 import React from "react";
+import { useContext } from "react";
 
+import CartContext from "../../store/cart-context";
 import classes from './HeaderCartButton.module.css'
 import CartIcon from "../Cart/CartIcon";
 
 const HeaderCartButton = props => {
+    const cartCtx = useContext(CartContext);
+
+    const numOfCartItems = cartCtx.items.reduce((currNum, item) => {
+        return currNum + item.amount;
+    }, 0);
+
     return <button className={classes.button} onClick={props.onClick}>
         <span className={classes.icon}>
             <CartIcon />
         </span>
         <span>Your Cart</span>
         <span className={classes.badge}>
-            3
+            {numOfCartItems}
         </span>
 
     </button>
